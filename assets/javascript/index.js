eaf06eec50c23f3a2441c6666efdaef13819b6ec
clicktapsolutions <marketing@clicktapsolutions.com>
2024-08-07 17:04:05 -0700

    tawk to integrated and contact form updated

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -70,7 +70,16 @@ document.addEventListener('DOMContentLoaded', () => {
       observer.observe(steps);
   });
 });
-
+///////////////////////////////////////////////////////
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/66b1d92532dca6db2cba7856/1i4jbgfg0';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
 
 
 
